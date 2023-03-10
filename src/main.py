889a91444cde0b4d03f8a1f892c1fc7fc9d928be
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-03-10 11:53:44 +0100

    ingenting funker

--- src/main.py
+++ src/main.py
@@ -1,19 +1,27 @@
-import sys
-from ldap3 import Server, Connection, ALL, NTLM, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES, AUTO_BIND_NO_TLS, SUBTREE
-from ldap3.core.exceptions import LDAPCursorError
+from pyad import *
 
+pyad.set_defaults(ldap_server="kuben.it", username="simen", password="Q2w3e4r5")
 
+q = adquery.ADQuery()
+q.execute_query(
+    attributes=["sAMAccountName", "givenName", "sn", "mail"],
+    where_clause="objectClass='user'"
+)
 
+# Print user information
+for user in q.get_results():
+    print(user["sAMAccountName"], user["givenName"], user["sn"], user["mail"])
 
+"""
 server_name = "odin.kuben.it"
 domain_name = "odin.kuben.it"
 user_name = "simen"
 password = "Q2w3e4r5"
 
-"""
+
 format_string = '{:25} {:>6} {:19} {:19} {}'
 print(format_string.format('User', 'Logins', 'Last Login', 'Expires', 'Description'))
-"""
+
 
 server = Server(server_name, get_info=ALL)
 conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
@@ -21,7 +29,7 @@ conn.search('dc=odin,dc=kuben,dc=it', '(GivenName=Alf)')
 
 print(conn)
 
-"""
+
 domain = ADDomain('kuben.it')
 ldap = domain.get_ldap_uris()
 ldap2 = domain.get_ldap_servers()
